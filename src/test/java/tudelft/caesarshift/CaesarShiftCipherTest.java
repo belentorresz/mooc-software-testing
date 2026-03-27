8bb22f981f0f334c6585b49f5d4e0a3da18f0335
Belen Torres <139027262+belentorresz@users.noreply.github.com>
2026-03-27 14:52:21 -0500

    se agrego test en CaesarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,37 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class CaesarShiftCipherTest {
 
+    @Test
+    void simpleShift() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        Assertions.assertEquals("def", cipher.encrypt("abc", 3));
+    }
+
+    @Test
+    void wrapAroundAlphabet() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        Assertions.assertEquals("abc", cipher.encrypt("xyz", 3));
+    }
+
+    @Test
+    void shiftWithSpaces() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        Assertions.assertEquals("ipmb nvoep", cipher.encrypt("hola mundo", 1));
+    }
+
+    @Test
+    void negativeShift() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        Assertions.assertEquals("abc", cipher.encrypt("def", -3));
+    }
+
+    @Test
+    void invalidCharacters() {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        Assertions.assertEquals("invalid", cipher.encrypt("hola123", 3));
+    }
 }
